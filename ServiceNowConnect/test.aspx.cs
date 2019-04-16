0f780a8967589f95250ec56e03c8910637aa7822
Daniel Denault <ddenault@appliedcloudsystems.com>
2019-04-16 19:18:23 -0400

    Add project files.

---
+++ ServiceNowConnect/test.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServiceNowConnect
+{
+	public partial class _default : System.Web.UI.Page
+	{
+		protected void Page_Load( object sender , EventArgs e )
+		{
+			try
+			{
+				Response.Write( "Connection String: " + Environment.GetEnvironmentVariable( "SQLAZURECONNSTR_DefaultConnectionString" ) );
+			}
+			catch( Exception ex )
+			{
+				Response.Write( ex.ToString() );
+			}
+		}
+	}
+}
